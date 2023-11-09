069bb1dcc3677ecdeec0b2cf5750457a0b5a5d1f
= <luucas.gomeswk@gmail.com>
2023-11-09 11:46:19 -0300

    Nova abordagem utilizando duas chains com prompts diferentes

--- Main.py
+++ Main.py
@@ -1,16 +1,15 @@
-from utils.Loader import load_config
+from utils.config_loader import load_config
 from model.Model import *
 import os
 import warnings  
 warnings.filterwarnings("ignore")  
-from utils.Logger import save_log
 
 
 if __name__ == "__main__":
     #ALTERAR DEPOIS
-    os.environ['HUGGINGFACEHUB_API_TOKEN'] = ''
+    os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
     config = load_config("model/config.json")
-    model, prompt, = config["model"], config["prompt"] 
+    model = config["model"]
     temperature, max_lenght = float(config["temperature"]), int(config["max_lenght"])
     searcher = Model(model, temperature, max_lenght)
 
@@ -18,4 +17,4 @@ if __name__ == "__main__":
     while True:
         search = input("Digite sua busca: ")
         anwser = searcher.run(search)
-        print("Produtos recomendados: " + anwser)
+        print("Produtos recomendados: ", anwser)
--- model/Model.py
+++ model/Model.py
@@ -1,14 +1,13 @@
 from langchain.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from utils.Cleaner import answer_cleaner
-from utils.Logger import save_log
+from utils.answer_cleaner import answer_cleaner
+from utils.log_saver import save_log
 
 """
 Documetação da classe Model:
     Atributos:
         model_name: Nome do modelo a ser utilizado.
-        prompt_template: Template do prompt a ser utilizado.
         temperature: Temperatura a ser utilizada no modelo.
         max_lenght: Tamanho máximo da resposta a ser gerada pelo modelo.
         
@@ -31,15 +30,39 @@ class Model():
         Resposta: Toalha, Saboneteira, Porta papel higiênico
 
         {pergunta}"""
+        self.PROMPT_TEMPLATE2 = """Com base nas informações fornecidas na busca do usuário, recomende uma lista com nomes de produtos que mais estejam relacionados com o contexto fornecido.
+        Não complete a entrada do usuário, somente relacione a frase de entrada com produtos interessantes no contexto.
+        Caso seja informado, considere características específicas como cores e tamanhos para acertar a resposta, conforme o exmplo 3.
+        Se você não souber a resposta, apenas diga que não sabe, não tente inventar uma resposta.
+
+        exemplo 1:
+        Pergunta: Quero comprar móveis para sala de estar
+        Resposta: Sofá, Televisão, Tapete, Mesa de Jantar
+
+        exemplo 2:
+        Pergunta: Preciso decorar meu banheiro
+        Resposta: Toalha, Saboneteira, Porta papel higiênico
+
+        exemplo 3:
+        Pergunta: Quero cobertas verdes
+        Resposta: Lençol verde, fronhas verdes, cobertor verde, colcha verde
+
+        {pergunta}"""
 
         self.prompt = PromptTemplate(template=self.PROMPT_TEMPLATE, input_variables=["pergunta"])
+        self.prompt2 = PromptTemplate(template=self.PROMPT_TEMPLATE2, input_variables=["pergunta"])
+
         self.llm = HuggingFaceHub(repo_id=model_name,
                                     model_kwargs={"temperature": temperature, "max_length": max_lenght})
 
         self.llm_chain = LLMChain(prompt=self.prompt, llm=self.llm)
+        self.llm_chain2 = LLMChain(prompt=self.prompt2, llm=self.llm)
     
     def run(self, input):
         answer = self.llm_chain.run(input)
+        answer2 = self.llm_chain2.run(input)
         clean_answer = answer_cleaner(answer)
-        save_log(input, clean_answer)
-        return clean_answer
+        clean_answer2 = answer_cleaner(answer2)
+        result = list(set(clean_answer + clean_answer2))
+        save_log(input, result)
+        return result
